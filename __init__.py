2733d53b68a8780c54c11d923303a7d72d774bdd
luc_wl <46969344+LucIJspeert@users.noreply.github.com>
2020-11-16 14:42:27 +0100

    small change in relative imports, now allowing direct access to eclipse_finding functions

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@
 Code written by: Luc IJspeert
 """
 
-from . import eclipse_finding as ecf
+from . import eclipse_finding
+from .eclipse_finding import *
 from . import plot_tools
 from . import set_tools

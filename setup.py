0f3a5a990002d1127daf52ebd49bc68b24a88232
thissop <600018686@fjuhsd.org>
2022-08-05 00:30:09 -0400

    updated and ready for pr

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name="eclipsr",
+    version='0.1',
+    author='Luc IJspeert',
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    package_data={'': ['tess_sectors.dat']},
+    include_package_data=True,
+    install_requires=['numpy', 'scipy', 'numba', 'astropy', 'matplotlib', 'h5py'])
+
+'''
 from distutils.core import setup
 
 setup(
@@ -8,3 +21,4 @@ setup(
     long_description=open('README.md').read(),
     install_requires = ['numpy', 'scipy', 'numba', 'astropy', 'matplotlib', 'h5py']
 )
+'''

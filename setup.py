d4c65d14e2358ac72521212db51ef6faf9bd42fd
thissop <600018686@fjuhsd.org>
2022-08-05 00:32:31 -0400

    slight change

--- setup.py
+++ setup.py
@@ -9,16 +9,3 @@ setup(name="eclipsr",
     package_data={'': ['tess_sectors.dat']},
     include_package_data=True,
     install_requires=['numpy', 'scipy', 'numba', 'astropy', 'matplotlib', 'h5py'])
-
-'''
-from distutils.core import setup
-
-setup(
-    name='eclipsr',
-    version='0.1',
-    author='Thaddaeus Kiker',
-    license='GNU General Public License v3.0',
-    long_description=open('README.md').read(),
-    install_requires = ['numpy', 'scipy', 'numba', 'astropy', 'matplotlib', 'h5py']
-)
-'''

c2134f4b96813d690a1c9b11d43f940b92182757
luc_wl <46969344+LucIJspeert@users.noreply.github.com>
2024-01-15 16:43:31 +0100

    fix

--- eclipsr/eclipse_finding.py
+++ eclipsr/eclipse_finding.py
@@ -1982,7 +1982,7 @@ def find_eclipses(times, signal, mode=1, max_n=80, tess_sectors=False, rf_classi
             score, attr_0, attr_1, attr_2, attr_3, attr_4, penalty = attrs
             rfc_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'random_forrest.dump')
             rfc = joblib.load(rfc_file)
-            score = rfc.predict(np.array(attrs[1:][np.newaxis]))
+            score = rfc.predict(np.array(attrs[1:])[np.newaxis])
         else:
             # legacy mode with the manually designed eclipse score
             score = eclipse_score(times, signal_s, r_derivs[0], period, ecl_indices, ecl_mid,
